3500aff3c60a4fcd5bc93117f7e2e9ed3e29af92
SaviorLeader <obitotobi569@gmail.com>
2026-01-27 16:12:11 +0400

    brrrr

---
+++ copy.py
@@ -0,0 +1,16 @@
+mport random
+students =[["nicaaaat",200],["broshka",380],["araaa",100],["fara",220],["abdulah",100]]
+gifts =["iphone 17","iphone 17 pro","iphone 17 pro max"]
+university1= "baku state university"
+university2= "azmiu"
+university3= "aztu"
+for student in students: 
+    if student[1] >= 350:
+        gifts = random.choice(gifts)
+        print("congrulations",student[0],"you got into",university3,"and you won",gifts)
+    elif student[1] >= 200:
+        print("congrulations",student[0],"you got into",university1)
+    elif student[1] >= 210:
+        print("congrulations",student[0],"you got into",university2)
+    else:
+        print("sen getdin bloka",student[0])
